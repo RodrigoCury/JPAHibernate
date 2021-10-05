592a230400c796cc925ac673abd6b60c7e8f2ae8
Rodrigo Cury <rodrigocury@ufu.br>
2021-10-05 18:39:39 -0300

    Test Lazy laod

--- JPAHibernate/src/main/java/br/dev/rodrigocury/loja/main/TestaPerformance.java
+++ JPAHibernate/src/main/java/br/dev/rodrigocury/loja/main/TestaPerformance.java
@@ -1,6 +1,9 @@
 package br.dev.rodrigocury.loja.main;
 
+import br.dev.rodrigocury.loja.DAO.PedidoDao;
+import br.dev.rodrigocury.loja.DAO.ProdutoDao;
 import br.dev.rodrigocury.loja.modelo.Pedido;
+import br.dev.rodrigocury.loja.modelo.Produto;
 import br.dev.rodrigocury.loja.util.JPAUtil;
 
 import javax.persistence.CascadeType;
@@ -11,8 +14,11 @@ public class TestaPerformance {
     public static void main(String[] args) {
         Cadastrador.populaBD();
         EntityManager em = JPAUtil.getEntity();
+        PedidoDao pedidoDao= new PedidoDao(em);
 
-        em.find(Pedido.class, 1L);
+        System.out.println(em.find(Produto.class, 1L));
+
+        System.out.println(pedidoDao.buscarPedidoComCliente(5L));
 
 
     }
